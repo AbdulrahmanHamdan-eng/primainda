adab92b103f90a6975984de03ec2f6178f6659f6
abdulrahman <hamonyamien@gmail.com>
2021-12-09 15:54:28 +0530

    new changes added

--- src/Section.js
+++ src/Section.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Section = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return <div>dfdfd</div>;
+};
 
-export default Section
+export default Section;
